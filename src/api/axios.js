1014d679314f5f156d3566661c10853b0ee777c0
Christopher Adekunle <72215147+Deoris06@users.noreply.github.com>
2023-01-12 23:28:10 +0100

    Added url

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 
 
 export default axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "http://phpstack-915447-3181078.cloudwaysapps.com",
     withCredentials: true
 })
